c9e15f02aaaf99be8cdaaeeb5d72479d58d1d4ea
Crlss123 <camadortjr@gmail.com>
2025-03-30 10:29:02 -0700

    Añade endpoint PATCH para actualizar partes de un evaluado dada su ID como parametro

--- index.ts
+++ index.ts
@@ -13,7 +13,11 @@ app.post("/evaluado/createevaluado", (req: Request, res: Response) => {
   res.send(`Crea un nuevo evaluado con el id ${id}`);
 });
 
-
+app.patch("/evaluado/patchevaluado/:id", (req: Request, res: Response) => {
+  res.send(
+    `Actualiza estas propiedades del evaluado con el id ${req.params.id}: status: ${req.body.status}, nombre: ${req.body.nombre}`
+  );
+});
 
 app.listen(PORT, () => {
   console.log("El servidor esta corriendo en el puerto 3000");
