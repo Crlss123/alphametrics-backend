eb1bb6b5e53f116461497537f969cbc04ca0f91f
Kimberly G. Marquez Preciado <A01255262@tec.mx>
2025-04-01 00:47:45 -0700

    Crear modelo person.

---
+++ src/models/person.ts
@@ -0,0 +1,59 @@
+import {Table, Model, Column, DataType, Unique} from 'sequelize-typescript';
+import {Optional} from 'sequelize';
+
+interface PersonAttributes{
+  id: number;
+  first_name: string;
+  second_name?: string;
+  first_lastname: string;
+  second_lastname: string;
+  curp: string;
+  genero: string;
+  graduado: boolean;
+}
+
+interface PersonCreationAttributes extends Optional<PersonAttributes, 'id'>{}
+
+@Table ({
+  tableName: "Evaluados",
+  timestamps: true,
+})
+export class Person extends Model<PersonAttributes, PersonCreationAttributes>{
+ 
+   @Column({
+      type: DataType.STRING(50),
+   })
+   first_name!: string;
+
+   @Column({
+      type: DataType.STRING(50),
+   })
+   second_name?: string;
+
+   @Column({
+    type: DataType.STRING(50),
+   })
+   first_lastname!: string;
+
+   @Column({
+    type:DataType.STRING(50),
+   })
+   second_lastname!: string;
+   
+   @Unique
+   @Column({
+    type:DataType.STRING(18),
+   })
+   curp!: string;
+
+   @Column({
+    type:DataType.STRING(1),
+   })
+   genero!: string;
+
+   @Column({
+    type:DataType.BOOLEAN,
+    defaultValue:false,
+   })
+   graduado!:boolean;
+}
