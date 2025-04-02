8f513423b3aa30e9ac73695e2fedd1b50a7e9cfe
Kimberly G. Marquez Preciado <A01255262@tec.mx>
2025-04-01 23:56:01 -0700

    Agregar controlador de tipo post.

---
+++ src/controllers/personController.ts
@@ -0,0 +1,30 @@
+import { Request, Response, RequestHandler } from "express";
+import { Person } from "../models/person";
+import { Zone } from "../models/zone";
+
+export const createPerson: RequestHandler = (req:Request, res:Response) => {
+    if(!req.body){
+        res.status(400).json({
+            status: "error",
+            message:"Content can not be empty",
+            payload: null,
+        });
+    }
+
+    const person = { ...req.body};
+    Person.create(person)
+    .then((data:Person | null)=> {
+        res.status(200).json({
+            status: "success",
+            message: "Product successfully created",
+            payload: data,
+        });
+    })
+    .catch((err)=>{
+        res.status(500).json({
+            status: "error",
+            message: "Something happened creating a product" + err.message,
+            payload: null,
+        });
+    });
+};
--- src/routers/personRouter.ts
+++ src/routers/personRouter.ts
@@ -1,9 +1,13 @@
 import { Router } from "express";
+import {
+    createPerson
+} from '../controllers/personController';
+
 const personRouter: Router = Router();
 
 personRouter.get("/",()=>{});
 
-personRouter.post("/",()=>{});
+personRouter.post("/",createPerson);
 
 personRouter.patch("/:id",()=>{});
 
